d1881b74b2f3f2b2e20fd695390bb371a27448c3
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-06-04 10:32:41 -1000

    added player input to move

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Mover : MonoBehaviour
-{
-
-    [SerializeField] float xValue = 0;
-    [SerializeField] float yValue = 0.02f;
-    [SerializeField] float zValue = 0;
-
+{    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +15,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, 0, zValue);
     }
 }
