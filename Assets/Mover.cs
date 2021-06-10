9fda0383543293d3cad77850d8d8e26b2d645f2a
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-06-10 09:42:28 -1000

    printing out instructions for game

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -11,15 +11,27 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't the hit walls!");
+    }
+
+    private void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
         transform.Translate(xValue, 0, zValue);
     }
 }
