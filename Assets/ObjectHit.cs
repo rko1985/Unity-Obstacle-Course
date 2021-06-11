65f50fda0f4b53e3623cc6096b903f78ef29da15
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-06-11 08:07:27 -1000

    collision when player bumps into wall

---
+++ Assets/ObjectHit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Bumped into a wall");
+    }
+
+}
